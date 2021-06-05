49c4740b23789488fea96d6ad58b1c98fdc97b79
maggie9600 <maheshraj9600@gmail.com>
2021-06-05 10:10:06 +0530

    First Commit

---
+++ chatapp/SQL/database.sql
@@ -0,0 +1,76 @@
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `chat`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chatrooms`
+--
+
+CREATE TABLE `chatrooms` (
+  `id` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `msg` varchar(200) NOT NULL,
+  `created_on` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chat_user_table`
+--
+
+CREATE TABLE `chat_user_table` (
+  `user_id` int(11) NOT NULL,
+  `user_name` varchar(250) NOT NULL,
+  `user_email` varchar(250) NOT NULL,
+  `user_password` varchar(100) NOT NULL,
+  `user_profile` varchar(100) NOT NULL,
+  `user_status` enum('Disabled','Enable') NOT NULL,
+  `user_created_on` datetime NOT NULL,
+  `user_verification_code` varchar(100) NOT NULL,
+  `user_login_status` enum('Logout','Login') NOT NULL,
+  `expDate` datetime NOT NULL,
+  `key` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `chatrooms`
+--
+ALTER TABLE `chatrooms`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `chat_user_table`
+--
+ALTER TABLE `chat_user_table`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `chatrooms`
+--
+ALTER TABLE `chatrooms`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `chat_user_table`
+--
+ALTER TABLE `chat_user_table`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
